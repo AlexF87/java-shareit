7f2a830a4282c11da9a99c01afef31f63c92a980
AlexF87 <feldman87@yandex.ru>
2022-11-29 15:46:15 +0700

    Добавить UserController

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,22 @@
 package ru.practicum.shareit.user;
 
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
+@Builder(toBuilder = true)
 public class User {
+    private Long id;
+    private String name;
+    @NotBlank
+    @NotNull
+    @Email
+    private String email;
 }
--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,12 +1,41 @@
 package ru.practicum.shareit.user;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 /**
  * TODO Sprint add-controllers.
  */
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(path = "/users")
 public class UserController {
+    private final UserService service;
+
+    @GetMapping
+    public Collection<UserDto> getUsers() {
+        return service.getUsers();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUserById(@PathVariable Long id) {
+        service.deleteUserById(id);
+    }
+
+    @GetMapping("/{id")
+    public UserDto getUser(@PathVariable Long id) {
+        return service.getUser(id);
+    }
+
+    @PostMapping
+    public UserDto addUser(@Valid @RequestBody UserDto userDto) {
+        return service.createUser(userDto);
+    }
+
+    @PatchMapping("/id")
+    public UserDto updateUser(@PatchVariable Long id, @RequestBody UserDto userDto) {
+        return service.updateUser(id, user);
+    }
 }
